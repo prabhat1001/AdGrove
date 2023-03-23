56015b2683bb698c3e83e00214e5cffe903fd09a
prabhat1001 <prabhatsaraswat24@gmail.com>
2023-03-23 11:08:39 +0530

    feat: Home page added

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// import components
+import ResponsiveAppBar from '../Components/ResponsiveAppBar';
+import LandingPage from '../Components/LandingPage';
+const Home = () => {
+  return (
+    <>
+       <ResponsiveAppBar /> 
+       <LandingPage />
+    </>
+  )
+}
+
+export default Home
